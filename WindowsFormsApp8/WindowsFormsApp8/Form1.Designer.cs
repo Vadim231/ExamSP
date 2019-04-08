c902005f8ee56c9acd59e8f7bb51d159a74774fa
Vadim231 <40603603+Vadim231@users.noreply.github.com>
2019-04-08 11:10:06 +0600

    Add files via upload

---
+++ WindowsFormsApp8/WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtBad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.pbFiles = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtReport = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStart.Location = new System.Drawing.Point(467, 415);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStartClick);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClose.Location = new System.Drawing.Point(713, 415);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnCloseClick);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBrowse.Location = new System.Drawing.Point(510, 77);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowseClick);
+            // 
+            // txtDir
+            // 
+            this.txtDir.Location = new System.Drawing.Point(168, 77);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(336, 20);
+            this.txtDir.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(41, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Directory Path";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(510, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnBrowse2Click);
+            // 
+            // txtBad
+            // 
+            this.txtBad.Location = new System.Drawing.Point(168, 139);
+            this.txtBad.Name = "txtBad";
+            this.txtBad.Size = new System.Drawing.Size(336, 20);
+            this.txtBad.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(41, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Vocabulary";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStop.Location = new System.Drawing.Point(592, 415);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // pbFiles
+            // 
+            this.pbFiles.Location = new System.Drawing.Point(103, 270);
+            this.pbFiles.Name = "pbFiles";
+            this.pbFiles.Size = new System.Drawing.Size(582, 36);
+            this.pbFiles.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(41, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Report Path";
+            // 
+            // txtReport
+            // 
+            this.txtReport.Location = new System.Drawing.Point(168, 200);
+            this.txtReport.Name = "txtReport";
+            this.txtReport.Size = new System.Drawing.Size(336, 20);
+            this.txtReport.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(510, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Browse...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnBrowse3Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtReport);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pbFiles);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBad);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDir);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Фильтруй базар";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStop;
+        public System.Windows.Forms.ProgressBar pbFiles;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtReport;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp8/WindowsFormsApp8/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        //Semaphore sema = new Semaphore(20,20);
+        bool isStopped = false;
+
+
+        //для потоков
+        public static int thrCount = 0;
+        public static ManualResetEvent mre;
+
+
+        //общий размер всех файлов
+        public static double totalSize = 0;
+
+        //папка в которой искать
+        DirectoryInfo directory = null;
+
+        //словарь со словами
+        FileInfo badFile = null;
+        public static string[] badWords = null;
+        public List<String> slova = new List<string>();
+
+        //папка для отчета
+        static public DirectoryInfo repDirectory = null;
+
+        Thread thSearch = null;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            pbFiles.Minimum = 0;
+            pbFiles.Step = 100;
+        }
+
+        public void btnStartClick(object sender, EventArgs e)
+        {
+            //Отчет
+            thSearch = new Thread(SearchRoutine);
+
+            btnStop.Enabled = true;
+            btnStart.Enabled = true;
+
+            thSearch.IsBackground = true;
+            thSearch.Start();
+        }
+
+        public void SearchRoutine()
+        {
+            mre = new ManualResetEvent(false);
+            //Словарик
+            if (badFile == null)
+            {
+                MessageBox.Show("Сначала выберите словарик", "Внимание!");
+                return;
+            }
+            using (StreamReader sr = new StreamReader(badFile.FullName))
+            {
+                while (!sr.EndOfStream)
+                    slova.Add(sr.ReadLine());
+            }
+            foreach (var word in slova)
+            {
+                badWords = word.Split(' ');
+            }
+
+
+            //создаем DirectoryInfo и указываем введенный в textBox путь.
+            if (directory == null)
+            {
+                MessageBox.Show("Сначала выберите папку!", "Внимание!");
+            }
+            else
+            {
+                try
+                {
+                    GetDirectorySize(directory);
+
+                    //MessageBox.Show("Общий размер файлов: " + totalSize.ToString() + " байт");
+                    
+                    
+
+                    //pbFiles.Maximum = Convert.ToInt32(totalSize);
+                    pbFiles.Invoke(new Action<int>((x) => { pbFiles.Maximum = x; pbFiles.Update(); }), Convert.ToInt32(totalSize));
+
+                    //pbFiles.Invoke(new Action<int>((x) => { pbFiles.Maximum = x; pbFiles.Update(); }),Convert.ToInt32(999000000));
+
+
+                    WalkDirectoryTree(directory, pbFiles);
+
+                    //конец работы
+
+                    btnStop.Invoke(new Action<bool>((x) => { btnStop.Enabled = x;}), false);
+
+                    btnStart.Invoke(new Action<bool>((x) => { btnStart.Enabled = x; }), false);
+                    
+                    DialogResult res=MessageBox.Show("Все файлы просмотренны, открыть отчет?","Работа завершена",MessageBoxButtons.OKCancel);
+                    if (res == DialogResult.OK)
+                    {
+                        Process.Start(repDirectory + @"\report.txt");
+                    }
+                    else
+                    {
+                        Close();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "SearchRoutine");
+                }
+            }
+        }
+
+
+        //Проходится по всем файлам и подпапкам в указанной папке
+        static public void WalkDirectoryTree(DirectoryInfo root,ProgressBar progress)
+        {
+            System.IO.FileInfo[] files = null;
+            System.IO.DirectoryInfo[] subDirs = null;
+
+            try
+            {
+                files = root.GetFiles("*.txt");
+            }
+            catch (Exception){ }
+            if (files != null)
+            {
+                foreach (FileInfo file in files)
+                {
+                    //полный путь файла
+                    string fileFullName = file.FullName;
+                    //размер файла
+                    double fileSize = Convert.ToDouble(file.Length);
+                    try
+                    {
+                        progress.Invoke(new Action<int>((x) => { progress.Value += x; progress.Update(); }), Convert.ToInt32(fileSize));
+                    }
+                    catch (Exception) { }
+                    try
+                    {
+                        //просматриваем каждый файл на наличие ПЛОХОГО СЛОВА
+
+                        string text2 = "";
+                        string copyText="";
+                        string text = File.ReadAllText(fileFullName);
+                        text2 = text.ToLower();
+
+                        foreach (var word in badWords)
+                        {
+                            if (text2.Contains(word))
+                            {
+                                //записываю в отчет инфу
+                                File.AppendAllText(repDirectory + @"\report.txt", "Путь файла: " + fileFullName+" Размер файла: "+file.Length + " Слово: " + word+"\r\n");
+                                //заменяем это слово на *****
+                                text2 = text2.Replace(word, "****");
+                                copyText = text2;
+
+                                //сохраняем файл с измененным текстом
+                                File.WriteAllText(repDirectory.FullName + @"\" + file.Name, copyText);
+                            }
+                            
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "WalkDirectoryTree");
+                    }
+                }
+                //подпапка
+                subDirs = root.GetDirectories();
+                foreach (DirectoryInfo dirInfo in subDirs)
+                {
+                    WalkDirectoryTree(dirInfo,progress);
+                }
+            }
+        }
+
+        static public void GetDirectorySize(DirectoryInfo root)
+        {
+            System.IO.FileInfo[] files = null;
+            System.IO.DirectoryInfo[] subDirs = null;
+
+            try
+            {
+                files = root.GetFiles("*.txt");
+            }
+            catch (Exception) { }
+            if (files != null)
+            {
+                foreach (FileInfo file in files)
+                {
+                    //полный путь файла
+                    string fileFullName = file.FullName;
+                    //размер файла
+                    double fileSize = Convert.ToDouble(file.Length);
+
+                    totalSize += fileSize;
+                    //progress.Invoke(new Action<int>((x) => { progress.Value = x; progress.Update(); }), Convert.ToInt32(fileSize));
+                }
+                //подпапка
+                subDirs = root.GetDirectories();
+                foreach (DirectoryInfo dirInfo in subDirs)
+                {
+                    GetDirectorySize(dirInfo);
+                }
+            }
+        }
+
+
+
+        //кнопка Close
+        private void btnCloseClick(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnBrowseClick(object sender, EventArgs e)
+        {
+            //выбрать папку, в которой будет поиск
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            //folder.RootFolder =  Environment.SpecialFolder.MyComputer;
+            try { folder.SelectedPath = @"D:\Домашки\test"; }
+            catch (Exception)
+            {
+                folder.SelectedPath = "C:\\";
+            }
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                txtDir.Text=folder.SelectedPath;
+                directory = new DirectoryInfo(folder.SelectedPath);
+            }
+        }
+
+        private void btnBrowse2Click(object sender, EventArgs e)
+        {
+            //выбор файла с плохими словами
+            OpenFileDialog file=new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                txtBad.Text = file.FileName;
+                badFile = new FileInfo(file.FileName);
+            }
+        }
+
+        private void btnBrowse3Click(object sender, EventArgs e)
+        {
+            //выбрать папку, в которой будет находиться отчет
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            try { folder.SelectedPath = @"D:\Домашки"; }
+            catch (Exception)
+            {
+                folder.SelectedPath = "C:\\";
+            }
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                txtReport.Text = folder.SelectedPath;
+                repDirectory = new DirectoryInfo(folder.SelectedPath);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            thrCount++;
+            if (thrCount % 2 != 0)
+            {
+                MessageBox.Show(thSearch.ThreadState.ToString());
+                try
+                {
+                    thSearch.Suspend();
+                }
+                catch (Exception) { }
+
+                btnStop.Text = "Resume";
+            }
+            else
+            {
+                try
+                {
+                    thSearch.Resume();
+                }
+                catch (Exception) { }
+                
+
+                btnStop.Text = "Stop";
+            }
+        }
+    }
+}
